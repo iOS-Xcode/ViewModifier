384acaa2746cddb45bf90ef350089e2a6e5abc9c
Seokhyun Kim <wjapp72@gmail.com>
2021-05-27 11:22:25 -0700

    Initial Commit

---
+++ ViewModifier/ViewModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifierApp.swift
+//  ViewModifier
+//
+//  Created by Seokhyun Kim on 2021-05-27.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
